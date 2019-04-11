87b91a4a12b692ac327e056676de54ff3cf49108
rodrigorgrb <rodrigorgrb@gmail.com>
2019-04-10 22:47:56 -0300

    Melhorando o random e o gera mapa

--- royale.rb
+++ royale.rb
@@ -11,37 +11,47 @@ def da_arma(arma)
     random = rand(arma.size)
     puts random
     arma = arma[random]
-    puts arma
-    arma
 end
 
 def sortea_arma
-    random = rand(6)
+    random = rand(5)
     puts random
     case random
-    when 1
+    when 0
         da_arma [['AWM',82],['VSS',50],['SKS',80]]
-    when 2
+    when 1
         da_arma [['M4A1',53],['Ak',61],['Famas',53]]
-    when 3
+    when 2
         da_arma [['MP5',48],['p90',48],['UMP',48]]
-    when 4
+    when 3
         da_arma [['SPAS',97],['Winchester87',94],['Cheline',100]]
-    when 5
+    when 4
         da_arma [['USP',45],['Desert Eagle',63],['3.8tão',50]]
     end
 end
 
+
 def sortea_obstaculos_premium
-  sortea_arma
+    random = rand(4)
+    puts random
+    case random
+    when 0
+        's'
+    when 1
+        'i'
+    when 2
+        'n'
+    when 3
+        'n'
+    end
 end
 
 def gera_mapa
   lateral = ""
   vertical = []
-  for x in 1..5
-    for x in 1..5
-      lateral += "_ "
+  for x in 1..10
+    for x in 1..10
+      lateral += sortea_obstaculos_premium+" "
     end
     vertical << lateral
     lateral = ""
@@ -50,9 +60,7 @@ def gera_mapa
   vertical
 end
 
-#name = da_boas_vindas
-#mapa = gera_mapa
-#puts mapa
+name = da_boas_vindas
+mapa = gera_mapa
+puts mapa
 arma = sortea_arma
-
-puts arma
